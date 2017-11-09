4579e947b98b813e64fe77b144629800057ae27b
Barry Hoggard <barry@tristanmedia.com>
2017-11-09 16:56:28 +0100

    WIP

--- smugmug-sync.rb
+++ smugmug-sync.rb
@@ -2,16 +2,31 @@
 # frozen_string_literal: true
 
 require 'dotenv'
-require 'oauth'
 require 'ruby-smugmug'
 require 'find'
 Dotenv.load
+
+$client = SmugMug::Client.new(
+  api_key: ENV['SMUGMUG_CONSUMER_KEY'],
+  oauth_secret: ENV['SMUGMUG_CONSUMER_SECRET'],
+  user: { token: ENV['SMUGMUG_ACCESS_TOKEN'], secret: ENV['SMUGMUG_ACCESS_SECRET']})
+
+$archive_category = client.categories.get.find { |c| c['Name'] == ENV['ARCHIVE_CATEGORY'] }
+
+# find the subcategory for a given year, or create it
+def find_subcategory(year)
+  subcategory = $client.subcategories.get(CategoryID: $archive_category['id']).find { |sc| sc['Name'] == year }
+  return subcategory if subcategory
+  subcategory = client.subcategories.create(CategoryID: 2538725754, Name: year)
+end
 
 def process_file(path)
   date = File.new(path).birthtime
   ymd = date.strftime('%Y-%m-%d')
   year = date.year
   puts "#{path}: #{year} #{ymd}"
+  subcategory = find_subcategory(year)
+  subcat_id = subcategory['id']
 end
 
 count = 0
